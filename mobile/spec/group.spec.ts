1c6e96dd4cf588196d5c5c7df443beede695ee19
bnanchen <nanchenbastian@gmail.com>
2019-11-06 11:39:01 +0100

    [group] test file for group/

---
+++ mobile/spec/group.spec.ts
@@ -0,0 +1,43 @@
+import { curve } from "@dedis/kyber";
+import Log from "../app/lib/cothority/log";
+import { KeyPair } from "../app/lib/dynacred/KeyPair";
+import { GroupDefinition } from "../app/lib/group/groupDefinition";
+import groupDefinitionList from "../app/lib/group/groupDefinitionCollection";
+
+const ed25519 = curve.newCurve("edwards25519");
+
+describe("Group Management", () => {
+    it("Test GroupDefinition", () => {
+        const user1 = new KeyPair();
+        const user2 = new KeyPair();
+
+        // creation of the first group definition
+        const originalVar = {
+            orgPubKeys: [user1._public, user2._public],
+            suite: ed25519,
+            voteThreshold: 50.0,
+            purpose: "Test",
+        };
+        const originalGD = new GroupDefinition(originalVar);
+
+        // user1 exchange the first group definition
+        originalGD.addSignature(user1._private);
+
+        // user2 receives the group definition by JSON
+        const jsonGD: string = originalGD.toJSON();
+        const user2OriginalGD = GroupDefinition.createFromJSON(jsonGD);
+
+        // user2 accepts the group definition
+        user2OriginalGD.addSignature(user2._private);
+
+        expect(user2OriginalGD.validate()).toBeTruthy();
+        expect(user2OriginalGD.verify()).toBeTruthy();
+
+        Log.print("Test passed!");
+    });
+    it("Test GroupDefinitionList", () => {
+        const gdl = groupDefinitionList.getInstance();
+
+        Log.print("Test passed!");
+    });
+});
